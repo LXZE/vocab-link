d8fe6d77e11466490397a6cc983365b9095ca764
Paul Lim <hayashi.azi1mov@outlook.com>
2023-10-11 22:15:44 +0700

    Add analytics

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
+  import * as amplitude from '@amplitude/analytics-browser';
   import Pane from '@/components/pane.svelte';
+  amplitude.init(import.meta.env['VITE_ANALYTICS_KEY'], {
+    defaultTracking: true,
+  });
 </script>
 
 <main class='w-full h-full'>
